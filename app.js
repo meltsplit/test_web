2f3ecdcf3f9e35bef02fa402d7362641bbe6a9e8
Seokwoo Jang <57269348+meltsplit@users.noreply.github.com>
2024-11-10 05:13:55 +0900

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ function App() {
   return (
     <div className="App">
       <h1>Hello from App.js!</h1>
+      {/* 강아지 이미지 추가 */}
+      <img 
+        src="https://upload.wikimedia.org/wikipedia/commons/3/3a/Golden_Retriever_puppy.jpg" 
+        alt="강아지 이미지" 
+        style={{ maxWidth: '100%', height: 'auto' }}
+      />
     </div>
   );
 }
